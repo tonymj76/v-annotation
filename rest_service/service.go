fb257d11807d0968375b29cc3ef88e57f7066703
tonymj76 <demeyortj@gmail.com>
2022-10-30 03:39:05 +0100

    service

---
+++ rest_service/service.go
@@ -0,0 +1,118 @@
+package rest_service
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tonymj76/video-annotator/datastore"
+	"github.com/tonymj76/video-annotator/model"
+	"github.com/tonymj76/video-annotator/utility"
+	"net/http"
+)
+
+type RESTServices struct {
+	repository datastore.Repository
+}
+
+func NewRESTServices(datastore datastore.Repository) *RESTServices {
+	return &RESTServices{
+		repository: datastore,
+	}
+}
+
+func (rs *RESTServices) CreateVideo(ctx *gin.Context) {
+	//s, _ := rs.repository.Create(ctx)
+	var schema model.AnnotatedVideo
+	if err := utility.ReadJSON(ctx, &schema); err != nil {
+		JSON(ctx, "failed", http.StatusBadRequest, err)
+		return
+	}
+	videoDetails, err := utility.NewVideoDetails(schema.VideoLink)
+	if err != nil {
+		JSON(ctx, "failed to get video details", http.StatusBadRequest, err)
+		return
+	}
+	schema.Duration = videoDetails.Duration
+	schema.FrameRate = videoDetails.Framerate
+	savedSchema, err := rs.repository.Create(ctx, &schema)
+	if err != nil {
+		JSON(ctx, "failed to save video details", http.StatusBadRequest, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusCreated, savedSchema)
+}
+
+func (rs *RESTServices) FetchVideos(ctx *gin.Context) {
+	videoSchema, err := rs.repository.FetchVideos(ctx)
+	if err != nil {
+		JSON(ctx, "failed to fetch videos", http.StatusInternalServerError, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, videoSchema)
+}
+
+func (rs *RESTServices) FetchVideo(ctx *gin.Context) {
+	videoSchema, err := rs.repository.FetchVideoByID(ctx)
+	if err != nil {
+		JSON(ctx, "failed to fetch videos", http.StatusInternalServerError, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, videoSchema)
+}
+
+func (rs *RESTServices) FetchSegmentByID(ctx *gin.Context) {
+	segment, err := rs.repository.FetchSegmentByID(ctx)
+	if err != nil {
+		JSON(ctx, "failed to fetch segments", http.StatusInternalServerError, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, segment)
+}
+
+func (rs *RESTServices) FetchSegments(ctx *gin.Context) {
+	segments, err := rs.repository.FetchSegments(ctx)
+	if err != nil {
+		JSON(ctx, "failed to fetch segments", http.StatusInternalServerError, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, segments)
+}
+
+func (rs *RESTServices) CreateSegment(ctx *gin.Context) {
+	videoSchema, err := rs.repository.FetchVideoByID(ctx)
+	if err != nil {
+		JSON(ctx, "failed to fetch videos", http.StatusInternalServerError, err)
+		return
+	}
+	var segments model.AnnotationSegments
+	if err := utility.ReadJSON(ctx, &segments); err != nil {
+		JSON(ctx, "failed", http.StatusBadRequest, err)
+		return
+	}
+	if err := utility.ValidateData(videoSchema, segments); err != nil {
+		JSON(ctx, "failed to validate segments", http.StatusBadRequest, err)
+		return
+	}
+	result, err := rs.repository.CreateSegmentByID(ctx, &segments)
+	if err != nil {
+		JSON(ctx, "failed to save segment", http.StatusBadRequest, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusCreated, result)
+}
+
+func (rs *RESTServices) DeleteVideo(ctx *gin.Context) {
+	value, err := rs.repository.DeleteVideo(ctx)
+	if err != nil {
+		JSON(ctx, "failed to delete video", http.StatusBadRequest, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, value)
+}
+
+func (rs *RESTServices) DeleteSegmentByID(ctx *gin.Context) {
+	value, err := rs.repository.DeleteSegmentByID(ctx)
+	if err != nil {
+		JSON(ctx, "failed to delete segment", http.StatusBadRequest, err)
+		return
+	}
+	JSON(ctx, "success", http.StatusOK, value)
+}
